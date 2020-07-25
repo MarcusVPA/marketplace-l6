8ff611639ae2f8029a96d2eec788eb38cf79fc0d
Marcus Vinicius <marcus_mayme@hotmail.com>
2020-07-25 18:54:21 -0300

    add produto carrinho

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -7,9 +7,25 @@ use Illuminate\Http\Request;
 class CartController extends Controller
 {
     //
+    public function index()
+    {
+        dd(session()->get('cart'));
+    }
+
     public function add(Request $request)
     {
         $product = $request->get('product');
-        dd($product);
+        
+        //verificar se existe sessao para os produtos
+        if(session()->has('cart')){
+            // adiciona os produtos na sessão existente
+            session()->push('cart',$product); // chave,valor que vai adicionar
+        }else{
+            // cria a sessão com o primeiro produto
+            $products[] = $product;
+            session()->put('cart',$products); // chave,valor que vai adicionar
+        }
+        flash('Produto Adicionado no carinho!')->success();
+        return redirect()->route('product.single',['slug'=>$product['slug']]);
     }
 }
